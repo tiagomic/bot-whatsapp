18f5c58827774bb05684d7b4b3a92743071e8bfc
tiagomic <tiago-michenko@hotmail.com>
2025-07-12 11:42:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ safety_settings = [
   },
 ]
 
-model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 
