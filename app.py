a9a5718cedd451f04017af03935388f065fb8c62
tiagomic <tiago-michenko@hotmail.com>
2025-07-12 12:02:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ safety_settings = [
     {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
     {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
 ]
-model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest", generation_config=generation_config, safety_settings=safety_settings)
+model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest", generation_config=generation_config, safety_settings=safety_settings)
 
 # Histórico de conversas
 conversation_history = {}
